9e48c894cfbcfc9a6bd2dd09a76d8530204eb42e
mauryadevang27-design <mauryadevang27@gmail.com>
2026-08-01 00:27:54 +0530

    Time: 13 ms (82.1%), Space: 41.9 MB (85.08%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -0,0 +1,19 @@
+import java.lang.Math;
+class Solution {
+    public int countTriples(int n) {
+        int res=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            { int sum = i*i+j*j;
+            int c = (int)Math.sqrt(sum);
+                if(c<=n && c*c==sum)
+                {
+                    res++;
+                }
+            }
+
+        }
+        return res;
+    }
+}
