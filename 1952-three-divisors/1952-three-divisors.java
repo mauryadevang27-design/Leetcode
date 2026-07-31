a99581993ed73baea8cc2d89e91417fca9268d9d
mauryadevang27-design <mauryadevang27@gmail.com>
2026-07-31 23:21:21 +0530

    Time: 1 ms (78.38%), Space: 42.1 MB (76.64%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isThree(int n) {
+        int count=1;
+    for(int i=2;i<=n;i++)
+    {
+        if(n%i==0)
+        
+            count++;
+        
+    }
+    if(count==3)
+    
+        return true;
+    else
+       return false;
+    
+}
+}
